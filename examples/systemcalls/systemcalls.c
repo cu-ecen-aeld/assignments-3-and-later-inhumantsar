0cc396a614659cfe76d570199b18063bac9bbd1a
Shaun Martin <inhumantsar@protonmail.com>
2025-12-22 21:45:55 +0000

    assignment 3 part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,12 @@
+#define _XOPEN_SOURCE
 #include "systemcalls.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -6,58 +14,94 @@
  *   successfully using the system() call, false if an error occurred,
  *   either in invocation of the system() call, or if a non-zero return
  *   value was returned by the command issued in @param cmd.
-*/
+ */
 bool do_system(const char *cmd)
 {
 
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
+    /*
+     * TODO  add your code here
+     *  Call the system() function with the command set in the cmd
+     *   and return a boolean true if the system() call completed with success
+     *   or false() if it returned a failure
+     */
 
-    return true;
+    const int ret = system(cmd);
+
+    if (ret == 0)
+        return true;
+    else
+        return false;
 }
 
 /**
-* @param count -The numbers of variables passed to the function. The variables are command to execute.
-*   followed by arguments to pass to the command
-*   Since exec() does not perform path expansion, the command to execute needs
-*   to be an absolute path.
-* @param ... - A list of 1 or more arguments after the @param count argument.
-*   The first is always the full path to the command to execute with execv()
-*   The remaining arguments are a list of arguments to pass to the command in execv()
-* @return true if the command @param ... with arguments @param arguments were executed successfully
-*   using the execv() call, false if an error occurred, either in invocation of the
-*   fork, waitpid, or execv() command, or if a non-zero return value was returned
-*   by the command issued in @param arguments with the specified arguments.
-*/
+ * @param count -The numbers of variables passed to the function. The variables are command to execute.
+ *   followed by arguments to pass to the command
+ *   Since exec() does not perform path expansion, the command to execute needs
+ *   to be an absolute path.
+ * @param ... - A list of 1 or more arguments after the @param count argument.
+ *   The first is always the full path to the command to execute with execv()
+ *   The remaining arguments are a list of arguments to pass to the command in execv()
+ * @return true if the command @param ... with arguments @param arguments were executed successfully
+ *   using the execv() call, false if an error occurred, either in invocation of the
+ *   fork, waitpid, or execv() command, or if a non-zero return value was returned
+ *   by the command issued in @param arguments with the specified arguments.
+ */
 
 bool do_exec(int count, ...)
 {
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char *command[count + 1];
     int i;
-    for(i=0; i<count; i++)
+    for (i = 0; i < count; i++)
     {
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    // command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
+    /*
+     * TODO:
+     *   Execute a system command by calling fork, execv(),
+     *   and wait instead of system (see LSP page 161).
+     *   Use the command[0] as the full path to the command to execute
+     *   (first argument to execv), and use the remaining arguments
+     *   as second argument to the execv() command.
+     *
+     */
+
+    if (!fork())
+    {
+        execv(command[0], command);
+        exit(EXIT_FAILURE);
+    }
+
+    int status;
+    const int pid = wait(&status);
+
+    // these conditionals adapted from LSP chapter 2
+    if (pid == -1)
+    {
+        perror("wait");
+        return false;
+    }
+    if (WIFEXITED(status) && status > 0)
+    {
+        printf("Exited with error: %d\n", status);
+        return false;
+    }
+    if (WIFSIGNALED(status))
+    {
+        printf("Killed by signal\n");
+        return false;
+    }
+    if (WIFSTOPPED(status))
+    {
+        printf("Stopped by signal\n");
+        return false;
+    }
 
     va_end(args);
 
@@ -65,17 +109,17 @@ bool do_exec(int count, ...)
 }
 
 /**
-* @param outputfile - The full path to the file to write with command output.
-*   This file will be closed at completion of the function call.
-* All other parameters, see do_exec above
-*/
+ * @param outputfile - The full path to the file to write with command output.
+ *   This file will be closed at completion of the function call.
+ * All other parameters, see do_exec above
+ */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char *command[count + 1];
     int i;
-    for(i=0; i<count; i++)
+    for (i = 0; i < count; i++)
     {
         command[i] = va_arg(args, char *);
     }
@@ -84,14 +128,78 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     // and may be removed
     command[count] = command[count];
 
+    /*
+     * TODO
+     *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
+     *   redirect standard out to a file specified by outputfile.
+     *   The rest of the behaviour is same as do_exec()
+     *
+     */
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
+    // Source - https://stackoverflow.com/a
+    // Posted by tmyklebu, modified by community. See post 'Timeline' for change history
+    // Retrieved 2025-12-22, License - CC BY-SA 3.0
+    int kidpid;
+    int fd = open(outputfile, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+
+    if (fd < 0)
+    {
+        perror("open");
+        abort();
+    }
+
+    switch (kidpid = fork())
+    {
+    case -1:
+        perror("fork");
+        abort();
+    case 0:
+        // child logic
+        // dup2 replaces file descriptor 1 (stdout) with fd so that
+        // anything that would have been written to stdout is written
+        // to the file instead.
+        // this doesn't affect the parent (or any other process) since
+        // each process gets its own set of file descriptors.
+        if (dup2(fd, 1) < 0)
+        {
+            perror("dup2");
+            abort();
+        }
+        // closing fd has no effect on the redirect since the stdout fd
+        // is entirely separate and pointing at our file.
+        close(fd);
+        execv(command[0], command);
+        // if execv is successful, these will never run
+        perror("execv");
+        abort();
+    default:
+        // parent logic
+        close(fd);
+        int status;
+        const int pid = wait(&status);
+
+        // these conditionals adapted from LSP chapter 2
+        if (pid == -1)
+        {
+            perror("wait");
+            return false;
+        }
+        if (WIFEXITED(status) && status > 0)
+        {
+            printf("Exited with error: %d\n", status);
+            return false;
+        }
+        if (WIFSIGNALED(status))
+        {
+            printf("Killed by signal\n");
+            return false;
+        }
+        if (WIFSTOPPED(status))
+        {
+            printf("Stopped by signal\n");
+            return false;
+        }
+    }
 
     va_end(args);
 
